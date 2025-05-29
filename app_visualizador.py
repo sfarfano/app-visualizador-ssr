9c03a6dddea0f84dc3e2ba0cb7e40c53a6c4852d
sfarfano <sfarfano@gmail.com>
2025-05-29 12:06:15 -0400

    Update app_visualizador.py

--- app_visualizador.py
+++ app_visualizador.py
@@ -127,7 +127,7 @@ if not st.session_state.autenticado:
         if autorizado:
             st.session_state.autenticado = True
             st.session_state.usuario = usuario.strip().lower()
-            st.experimental_rerun()
+            st.rerun()
         else:
             st.error("Usuario o PIN incorrecto.")
     st.stop()
